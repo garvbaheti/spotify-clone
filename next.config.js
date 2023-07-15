a540cf9470c6b726a10eb3dd8669bf3933d37a22
garvbaheti <garvbaheti@gmail.com>
2023-07-15 16:39:28 +0530

    final

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+       images:{
+              domains:[
+                     "ljulvxhusjsbklvidbjc.supabase.co"
+              ]
+       }
+}
+
+module.exports = nextConfig
